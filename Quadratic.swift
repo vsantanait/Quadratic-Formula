40c4c6d2d3cb6c824c21b935b3b3e57e674efb24
Vanessa Santana <vsantana.it@gmail.com>
2021-05-22 21:42:41 -0400

    Create Quadratic.swift

---
+++ Quadratic.swift
@@ -0,0 +1,18 @@
+var a: Double = 3.0
+var b: Double = -11.0
+var c: Double = -4.0
+
+var root1: Double = 0
+root1 = (b*b - 4*a*c)
+
+var root2: Double = 0
+root2 = (-b - (b*b - 4*a*c).squareRoot()) / (2*a)
+
+root1 = root1.squareRoot()
+root1 = -b + root1
+root1 = root1 / (2 * a)
+
+
+print("Root 1 is \(root1)")
+print("Root 2 is \(root2)")
+
